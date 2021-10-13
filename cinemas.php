8b026f865a7bfbdee1a16487585126a7c5c6ef59
Melvin Lim <melvin@pand.ai>
2021-10-14 01:34:12 +0800

    cinema page css responsive

--- cinemas.php
+++ cinemas.php
@@ -19,14 +19,25 @@
     <link rel="stylesheet" href="css/footer.css">
 
     <style>
+         .cinema-layout-wrapper {
+             display: grid;
+             grid-template-columns: 300px 1fr;
+             margin-top: 30px;
+             grid-gap: 150px;
+             justify-self: center;
+         }
          .cinema-location {
-            background-clip: border-box;
+            /*background-clip: border-box;*/
             background-color: rgb(38, 38, 38);
-            background-origin: padding-box;
+            /*background-origin: padding-box;*/
+            margin-top: 0;
+            margin-bottom: 0;
             background-size: auto;
-            line-height: 20px;
             width: 280px;
             padding: 15px;
+            box-shadow: 0px 0px 15px 6px #EA2127;
+            display: flex;
+            flex-direction: column;
         }
         .cinema-location li {
             list-style: none;
@@ -38,8 +49,6 @@
             background-clip: border-box;
             background-size: auto;
             background-origin: padding-box;
-            background-position-x: 0%;
-            background-position-y: 0%;
             border-radius: 10px;
             cursor: pointer;
             display: block;
@@ -52,6 +61,19 @@
         }
         li a:hover {
             color: red;
+        }
+        .experience-thumbnail {
+            display: inline-grid;
+            grid-template-columns: repeat(2, [col-start] 50%);
+            grid-gap: 70px;
+            /*margin-left: 100px;*/
+            justify-self: center;
+
+        }
+        .experience-thumbnail img {
+            width: 25vw;
+            height: 25vh;
+            box-shadow: 0px 0px 15px 6px #EA2127;
         }
     </style>
 </head>
@@ -72,30 +94,40 @@
             <strong class="current-page-breadcrumb">Cinemas</strong>
         </p>
         <h1>Our Cinemas</h1>
-        <?php
-        $servername = "localhost";
-        $username = "f32ee";
-        $password = "f32ee";
-        $dbname = "f32ee";
+        <div class="cinema-layout-wrapper">
+            <div>
+                <?php
+                $servername = "localhost";
+                $username = "f32ee";
+                $password = "f32ee";
+                $dbname = "f32ee";
 
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
 
-        $query_cinema_list = "
-        SELECT name
-        FROM
-        `Cinema`
-        ";
-        $cinema_list = mysqli_query($conn, $query_cinema_list);
-        echo "<ul class='cinema-location'>";
-        while ($row = mysqli_fetch_assoc($cinema_list)) {
-            echo '<li><a>Screens Come True, ' . $row["name"] . '</a>';
-        }
-        echo '</li>';
-        ?>
-
+                $query_cinema_list = "
+                SELECT name
+                FROM
+                `Cinema`
+                ";
+                $cinema_list = mysqli_query($conn, $query_cinema_list);
+                echo "<ul class='cinema-location'>";
+                while ($row = mysqli_fetch_assoc($cinema_list)) {
+                    echo '<li><a>Screens Come True, ' . $row["name"] . '</a>';
+                }
+                echo '</li>';
+                ?>
+            </div>
+            <div class="experience-thumbnail">
+                <img src="img/experience/thumbnail/thumbnail_dbox.jpg">
+                <img src="img/experience/thumbnail/thumbnail_dolby_atmos.jpg">
+                <img src="img/experience/thumbnail/thumbnail_imax.jpg">
+                <img src="img/experience/thumbnail/thumbnail_onyx_led.jpg">
+            </div>
+            <br><br><br>
+        </div>
     </div>
     <?php include "components/footer.html"; ?>
 </div>
