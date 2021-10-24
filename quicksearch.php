76448ef41068598c937b79740c3719e34b2eef2b
Huei Min Lee <hueimin426@gmail.com>
2021-10-24 12:57:01 +0800

    1024merge

--- quicksearch.php
+++ quicksearch.php
@@ -4,25 +4,35 @@ $date = $_POST["date"];
 $movie = $_POST["movie"];
 $showtime = $_POST["showtime"];
 $cinema = $_POST["cinema"];
-print($cinema);
-print($movie);
-print($showtime);
-print($date);
 
 
-$query = "
-SELECT Movie.id AS movieID, Cinema.id cinemaID, Cinema_Hall.cinemaHallID AS cinemaHallId
-FROM `Movie`, `Cinema`, `Cinema_Hall`
-WHERE Showtime.startTime='" . $date . $showtime .":00' AND 
-Movie.title='" . $movie . "' AND 
-Cinema.name='" . $cinema . "'
-";
+echo
+
+"SELECT Showtime.cinemaHallID AS cinemaHallID, Showtime.movieID AS movieID, Cinema.cinemaID AS cinemaID 
+FROM Showtime, Cinema WHERE Showtime.cinemaHallID IN (SELECT cinemaHallID FROM Cinema_Hall WHERE CinemaID = (SELECT cinemaID FROM Cinema WHERE name='".$cinema."')) 
+AND Showtime.startTime='".$date." ".$showtime.":00'
+AND Showtime.movieID = (SELECT movieID From Movie WHERE title='".$movie."') 
+AND Cinema.cinemaID = (SELECT cinemaID FROM Cinema WHERE name='".$cinema."'); ";
+
+$query = 
+"SELECT Showtime.cinemaHallID AS cinemaHallID, Showtime.movieID AS movieID, Cinema.cinemaID AS cinemaID 
+FROM Showtime, Cinema WHERE Showtime.cinemaHallID IN (SELECT cinemaHallID FROM Cinema_Hall WHERE CinemaID = (SELECT cinemaID FROM Cinema WHERE name='".$cinema."')) 
+AND Showtime.startTime='".$date." ".$showtime.":00'
+AND Showtime.movieID = (SELECT movieID From Movie WHERE title='".$movie."') 
+AND Cinema.cinemaID = (SELECT cinemaID FROM Cinema WHERE name='".$cinema."'); ";
 
 $resultQuery = $dbcnx->query($query);
 $numResultQuery = $resultQuery->num_row;
 for ($k=0; $k<$numResultQuery; $k++) {
+    echo 'hi';
     $row = $resultQuery->fetch_assoc();
-    print($row["movieID"]);
+    echo($row["movieID"]);
+
 }
 
+// echo 
+//         '<script>
+//             alert("Record updated successfully");
+//             window.location.href="product_price_update.php";
+//         </script>'
 ?>
