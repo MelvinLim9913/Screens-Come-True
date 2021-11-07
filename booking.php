dd680eafd3a4dab901c1ed60290a9859a0e115dd
Huei Min Lee <hueimin426@gmail.com>
2021-11-07 22:09:40 +0800

    final

--- booking.php
+++ booking.php
@@ -132,9 +132,9 @@
             padding-left: 330px;
             display: flex;
             align-items: center;
-            width: calc(100% - 370px);
-            margin-left: 20px;
-            margin-right: 20px;
+            width: calc(100% - 395px);
+            margin-left: 30px;
+            margin-right: 30px;
         }
         .step-caption {
             padding-left: 330px;
@@ -143,7 +143,7 @@
             width: calc(100% - 330px);
         }
         .step-caption h3{
-            width: 10%;
+            width: 13%;
             text-align: center;
             text-wrap: normal;
             color: #FFD60A;
@@ -254,9 +254,9 @@
                         <div class="circle">3</div>
                     </div>
                     <div class="step-caption">
-                        <h3 class="caption active">Seat<br>Selection</h3>
-                        <h3 class="caption">Enter<br>Particulars</h3>
-                        <h3 class="caption">Confirmation</h3>
+                        <h3 class="caption active">Select&nbsp;seats</h3>
+                        <h3 class="caption">Food &<br>Beverage</h3>
+                        <h3 class="caption">Payment&nbsp;&<br>Confirmation</h3>
                     </div>
                 </div>
                 <br>
